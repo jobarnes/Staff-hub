bbc3789d7bd9b121b48a8091171da7c505d6d497
Jo Barnes <jo.barnes@unimelb.edu.au>
2014-04-02 17:49:52 +1100

    Zendesk snippets

--- Snippets/Footer/local-functions.js
+++ Snippets/Footer/local-functions.js
@@ -2,3 +2,23 @@
     jQuery(document).ready(function($){
         $( ".link-block > ul > li:nth-child(3n)").after( "<hr>" );
     });
+    
+    
+<!-- Zendesk feedback tab -->
+<script type="text/javascript" src="//assets.zendesk.com/external/zenbox/v2.6/zenbox.js"></script>
+
+<script type="text/javascript">
+// <![CDATA[
+if (typeof(Zenbox) !== "undefined") {
+Zenbox.init({
+dropboxID: "20139974",
+url: "https://unimelbit.zendesk.com",
+tabTooltip: "Feedback",
+tabImageURL: "https://assets.zendesk.com/external/zenbox/images/tab_feedback_right.png",
+tabColor: "black",
+tabPosition: "Right",
+hide_tab: "true",
+});
+}
+// ]]>
+</script>
